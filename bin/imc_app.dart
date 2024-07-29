d2ac486d89598942fdaf7a14ab2568b17e1d7237
Zaguetto <fabio.zaguetto@gmail.com>
2024-07-29 13:53:11 -0300

    update ver. 1.0.2

--- bin/imc_app.dart
+++ bin/imc_app.dart
@@ -9,7 +9,7 @@ class Pessoa {
 
   double calcularIMC() {
     if (altura <= 0) {
-      throw Exception("Altura inválida. A altura deve ser maior que zero.");
+      throw ArgumentError("Altura inválida. A altura deve ser maior que zero.");
     }
     return peso / (altura * altura);
   }
@@ -18,19 +18,38 @@ class Pessoa {
 void main() {
   try {
     print("Digite o nome da pessoa:");
-    String nome = stdin.readLineSync()!;
+    String? nome = stdin.readLineSync();
+
+    if (nome == null || nome.isEmpty) {
+      throw Exception("Nome inválido. Por favor, insira um nome válido.");
+    }
 
     print("Digite o peso (em kg):");
     double peso = double.parse(stdin.readLineSync()!);
 
+    if (peso <= 0) {
+      throw ArgumentError("Peso inválido. O peso deve ser maior que zero.");
+    }
+
     print("Digite a altura (em metros):");
     double altura = double.parse(stdin.readLineSync()!);
+
+    if (altura <= 0) {
+      throw ArgumentError("Altura inválida. A altura deve ser maior que zero.");
+    }
 
     Pessoa pessoa = Pessoa(nome, peso, altura);
     double imc = pessoa.calcularIMC();
 
     print("O IMC de ${pessoa.nome} é: $imc");
-  } catch (e) {
-    print("Ocorreu um erro: $e");
+  } on FormatException catch (e) {
+    print("Erro de formatação: $e");
+  } on ArgumentError catch (e) {
+    print("Argumento inválido: $e");
+  } on Exception catch (e) {
+    print("Erro: $e");
+  } catch (e, s) {
+    print("Erro inesperado: $e");
+    print("Stack trace: $s");
   }
 }
